f857eaa58816c72890c167992aeb1310e3da5522
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-20 16:43:03 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,28 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+ number_of_times = 7
  phrase = "Welcome to Flatiron School's Web Development Course!"
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
