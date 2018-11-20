dc3662d3b9a872cdf6330475b3d2058a82dde204
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-20 16:21:13 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,6 +5,7 @@ def loop_iterator(number_of_times)
   counter = 0
   gets number_of_times
   loop do 
+    phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     counter += 1
     if counter == number_of_times
