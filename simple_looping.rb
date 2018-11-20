593ac08d6e0a5579afc8bac2b071ee992004c923
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-20 16:36:01 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,9 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   puts phrase
   counter +=1
-  
+  if counter == 7 
+    break
+  end
 end
 
 def times_iterator(number_of_times)
